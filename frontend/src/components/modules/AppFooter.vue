480bbd8bd65fe6e4c818be8a07401afbc56d61a5
Soma Keisuke <zero.one.mean.ability@outlook.jp>
2023-08-08 15:30:53 +0900

    あｄｄ

--- frontend/src/components/modules/AppFooter.vue
+++ frontend/src/components/modules/AppFooter.vue
@@ -1,11 +1,9 @@
 <template>
     <footer class="border-top">
         <b-nav align="center">
-            <b-nav-item active>Active</b-nav-item>
-            <b-nav-item>Link</b-nav-item>
-            <b-nav-item>Another Link</b-nav-item>
-            <b-nav-item disabled>Disabled</b-nav-item>
+            <div>
+                <b-button block variant="primary">アップロード一覧</b-button>
+            </div>
         </b-nav>
-        <p>cat</p>
     </footer>
 </template>
