234ee7dbb42386f2a107e7f2168df66626fe6384
Soma Keisuke <zero.one.mean.ability@outlook.jp>
2023-08-23 16:56:28 +0900

    yosagemaru

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,30 @@
 <template>
   <div id="app">
+    <Header />
     <div 
       id="nav" 
       class="tab-area-base">
       <ul class="tab-menu-base">
-        <!-- `router.js` で定義したルーティングルールとの紐付けを行っている -->
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/list">Rounting Test</router-link></li>
       </ul>
     </div>
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from './components/modules/AppHeader.vue';
+import Footer from './components/modules/AppFooter.vue';
 
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer
+  },
+};
 </script>
 
 <style>
--- frontend/src/components/modules/AppFooter.vue
+++ frontend/src/components/modules/AppFooter.vue
@@ -2,7 +2,7 @@
     <footer class="border-top">
         <b-nav align="center">
             <div>
-                <b-button block variant="primary">アップロード一覧</b-button>
+                <b-button block variant="primary" href="/list">アップロード一覧</b-button>
             </div>
         </b-nav>
     </footer>
